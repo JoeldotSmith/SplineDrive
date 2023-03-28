f2b9f44fff0100935493a10162339ac3f888632e
Joel Smith <joelsmith2004@gmail.com>
2023-03-28 04:14:35 +0000

    28 Mar 2023, 12:14

--- AUTO4508Lab3.c
+++ AUTO4508Lab3.c
@@ -2,9 +2,9 @@
 #include "math.h"
 #include <stdio.h>
 
-#define X_GOAL 400
-#define Y_GOAL 700
-#define ANGLE_GOAL -0
+#define X_GOAL 0
+#define Y_GOAL 1000
+#define ANGLE_GOAL 90
 #define INTERVAL 0.01
 #define SPEED 100
 #define ANGLESPEED 20
